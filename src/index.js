84b9a02e54534e663de6728e64ea736acefe30a8
namlev <mikipatkowski@protonmail.com>
2024-07-04 18:12:59 +0200

    Finish sidepanel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './styles.css'
 import EventIcon from './assets/img/event.svg'
+import CalendarClockIcon from './assets/img/calendar_clock.svg'
 
 const images = document.querySelectorAll('img');
 images[0].src = EventIcon;
+images[1].src = CalendarClockIcon;
