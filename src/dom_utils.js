13d852ceeaaf64b7896ca7239c2e90ae4d6e9468
namlev <mikipatkowski@protonmail.com>
2024-07-04 19:05:36 +0200

    Extract DOM elements to JS

---
+++ src/dom_utils.js
@@ -0,0 +1,89 @@
+export function createP(className, text){
+    const p = document.createElement('p');
+    p.classList.add(className)
+    p.textContent = text;
+    return p;
+}
+
+export function createDiv(className){
+    const div = document.createElement('div');
+    if (className)
+        div.classList.add(className)
+    return div;
+}
+
+export function createForm(className){
+    const form = document.createElement('form');
+    form.classList.add(className)
+    return form;
+}
+
+export function createH1(text){
+    const h1 = document.createElement('h1');
+    h1.textContent = text;
+    return h1;
+}
+
+export function createH2(text){
+    const h2 = document.createElement('h2');
+    h2.textContent = text;
+    return h2;
+}
+
+export function createH3(text){
+    const h3 = document.createElement('h3');
+    h3.textContent = text;
+    return h3;
+}
+
+export function createSelect(name, id, options){
+    const select = document.createElement('select');
+    select.name = name;
+    select.id = id;
+
+    for (let key in options) {
+            let option = document.createElement('option');
+            option.value = key;
+            option.textContent = options[key];
+            select.appendChild(option);
+    }
+
+    return select;
+}
+
+export function createDate(name, id){
+    const input = document.createElement('input');
+    input.type = 'date';
+    input.name = name;
+    input.id = id;
+    return input;
+}
+
+export function createTime(name, id){
+    const input = document.createElement('input');
+    input.type = 'time';
+    input.name = name;
+    input.id = id;
+    return input;
+}
+
+export function createBtn(type, className, text){
+    const btn = document.createElement('button');
+    btn.type = type;
+    btn.classList.add(className);
+    btn.textContent = text;
+    return btn;
+}
+
+export function createLi(className){
+    const li = document.createElement('li');
+    li.classList.add(className);
+    return li;
+}
+
+export function createSpan(className, text){
+    const span = document.createElement('span');
+    span.classList.add(className);
+    span.textContent = text;
+    return span;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,56 @@
 import './styles.css'
 import EventIcon from './assets/img/event.svg'
 import CalendarClockIcon from './assets/img/calendar_clock.svg'
+import {createDiv, createP, createSpan} from "./dom_utils";
 
-const images = document.querySelectorAll('img');
-images[0].src = EventIcon;
-images[1].src = CalendarClockIcon;
+const sideMenu = document.querySelector('#side_menu')
+const main = document.querySelector('#main')
+
+function createCategoryListElement(text){
+    const li = document.createElement('li');
+    const span = createSpan('clickable_text', text)
+    li.appendChild(span);
+    return li;
+}
+
+function addTodaysTasksPanel() {
+    const todayTasksCard = createDiv('side_menu_card')
+    const imgWrap = createDiv('img_wrap')
+    const img = document.createElement('img')
+    img.alt = 'Calendar icon'
+    img.src = EventIcon;
+    const p = createP('clickable_text', 'Today tasks')
+    const ul = document.createElement('ul');
+    const home = createCategoryListElement('Home')
+    const work = createCategoryListElement('Work')
+    const personal = createCategoryListElement('Personal')
+    ul.appendChild(home)
+    ul.appendChild(work)
+    ul.appendChild(personal)
+
+
+    imgWrap.appendChild(img);
+    todayTasksCard.appendChild(imgWrap);
+    todayTasksCard.appendChild(p)
+    todayTasksCard.appendChild(ul)
+    sideMenu.appendChild(todayTasksCard);
+}
+
+addTodaysTasksPanel();
+
+
+function addScheduledTasksPanel() {
+    const scheduledTasksCard = createDiv('side_menu_card')
+    const imgWrap = createDiv('img_wrap')
+    const img = document.createElement('img')
+    img.alt = 'Calendar icon'
+    img.src = CalendarClockIcon;
+    const p = createP('clickable_text', 'Scheduled tasks')
+
+    imgWrap.appendChild(img);
+    scheduledTasksCard.appendChild(imgWrap);
+    scheduledTasksCard.appendChild(p);
+    sideMenu.appendChild(scheduledTasksCard)
+}
+
+addScheduledTasksPanel();
