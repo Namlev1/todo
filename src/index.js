edf8beec097808fb0054e86b9ce5bd5bdc899532
namlev <mikipatkowski@protonmail.com>
2024-07-05 16:24:15 +0200

    Add date picker functionality

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ function renderSidePanel() {
 }
 
 renderSidePanel();
+
+const dateBtn = document.querySelector("#date_btn");
+const dateInput = document.querySelector("#date");
+const dateSpan = document.querySelector("#date_val");
+
+dateBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    dateInput.showPicker();
+})
+dateInput.addEventListener('change', function() {
+    let selectedDate = this.value;
+    console.log('Selected Date:', selectedDate);
+    dateSpan.textContent = selectedDate;
+});
