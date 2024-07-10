356ebbc886f33ff6c80e2864e774abea17e773ec
namlev <mikipatkowski@protonmail.com>
2024-07-10 15:48:44 +0200

    Change persisted data to 3 different variables

--- src/logic/storage.js
+++ src/logic/storage.js
@@ -1,20 +1,35 @@
 import {Task} from "./task";
 
+const taskTypes = {
+    home: [],
+    work: [],
+    personal: []
+}
+
 export function save(task){
     console.log(`saving task: ${JSON.stringify(task)})`)
-    localStorage.setItem(String(Task.id), JSON.stringify(task));
+    if (taskTypes[task.type]){
+        taskTypes[task.type].push(task)
+        localStorage.setItem(`${task.type}Tasks`, JSON.stringify(taskTypes[task.type]));
+    }
     Task.id++;
     localStorage.setItem("id", String(Task.id));
 }
 
 export function remove(task){
     console.log(`removing task: ${JSON.stringify(task)})`)
-    localStorage.removeItem(String(task.id));
+    if (taskTypes[task.type]){
+        const index = taskTypes[task.type].indexOf(arrTask => task.id === arrTask.id)
+        taskTypes[task.type].splice(index, 1);
+        localStorage.setItem('homeTasks', JSON.stringify(taskTypes[task.type]));
+    }
 }
 
 export function loadStorage(){
-    Task.id = localStorage.getItem("id") === null ? 1 : localStorage.getItem("id");
-    for (let i = 0; i < localStorage.length; i++){
-        console.log((localStorage.getItem(localStorage.key(i))));
-    }
+    Object.keys(taskTypes).forEach(type => {
+        taskTypes[type].push(JSON.parse(localStorage.getItem(`${type}Tasks`)))
+        console.log(`${type}: ${JSON.stringify(taskTypes[type])}`)
+    })
+    Task.id = localStorage.getItem('id') === null ? 1 : JSON.parse(localStorage.getItem('id'))
+    console.log(Task.id)
 }
