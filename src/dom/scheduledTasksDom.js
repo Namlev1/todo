f0caf0f68c11f76c55570f816282dab8261d88b7
namlev <mikipatkowski@protonmail.com>
2024-07-15 12:05:12 +0200

    Sort scheduled tasks

--- src/dom/scheduledTasksDom.js
+++ src/dom/scheduledTasksDom.js
@@ -2,7 +2,7 @@ import {deleteChildren} from "./domUtils";
 
 const main = document.querySelector('#main')
 
-export function displayScheduledTaskPanel(tasks){
+export function displayScheduledTaskPanel(tasks) {
     deleteChildren(main)
-
+    // tasks.forEach(task => {displayTask(task)})
 }
--- src/logic/scheduledTasks.js
+++ src/logic/scheduledTasks.js
@@ -1,6 +1,7 @@
 import {displayScheduledTaskPanel} from "../dom/scheduledTasksDom";
 import {taskTypes} from "./storage";
 import {compareTaskDates} from "../util/timeUtil";
+import {compareAsc, format, parseISO} from "date-fns";
 
 export function showScheduledTasks() {
     const scheduledTasks = getScheduledTasks();
@@ -13,7 +14,26 @@ function getScheduledTasks() {
         const typeTasks = taskTypes[type];
         scheduledTasks.push(...typeTasks);
     });
-    scheduledTasks.sort((a, b) => compareTaskDates(a, b))
-    console.log(`scheduled tasks: ${JSON.stringify(scheduledTasks)}`)
-    return scheduledTasks;
+
+    const tasksByDate = new Map();
+    scheduledTasks.forEach(task => {
+        const taskDate = format(parseISO(task.date), 'yyyy-MM-dd');
+        if (!tasksByDate.has(taskDate)) {
+            tasksByDate.set(taskDate, []);
+        }
+        tasksByDate.get(taskDate).push(task);
+    });
+
+    const groupedTasks = Array.from(tasksByDate.values());
+
+    groupedTasks.forEach(group => group.sort(compareTaskDates));
+    groupedTasks.sort((a, b) => {
+        const dateA = parseISO(a[0].date);
+        const dateB = parseISO(b[0].date);
+        return compareAsc(dateA, dateB);
+    });
+
+    console.log('Grouped tasks:', groupedTasks);
+
+    return groupedTasks;
 }
