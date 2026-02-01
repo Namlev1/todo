95e31f8e2deb8d35e6849c24c6e7dcf9a1e5547d
namlev <mikipatkowski@protonmail.com>
2026-02-01 11:33:32 +0100

    changes

--- src/dom/scheduledTasksDom.js
+++ src/dom/scheduledTasksDom.js
@@ -1,62 +1,70 @@
-import {createBtn, createDiv, createForm, createH2, createP, createRadioBtn, deleteChildren} from "./domUtils";
-import {Types} from "../logic/taskTypes";
-import {deleteTaskEventListener} from "../events/taskEventListeners";
-import {convertTo12HourFormat} from "../util/timeUtil";
-import {format, parseISO} from "date-fns";
+import { format, parseISO } from 'date-fns'
+import {
+  createBtn,
+  createDiv,
+  createForm,
+  createH2,
+  createP,
+  createRadioBtn,
+  deleteChildren
+} from './domUtils'
+import { Types } from '../logic/taskTypes'
+import { deleteTaskEventListener } from '../events/taskEventListeners'
+import { convertTo12HourFormat } from '../util/timeUtil'
 
 const main = document.querySelector('#main')
 
 export function displayScheduledTaskPanel(taskGroups) {
-    deleteChildren(main)
-    taskGroups.forEach(group => {
-        displayTaskGroup(group)
-    })
+  deleteChildren(main)
+  taskGroups.forEach(group => {
+    displayTaskGroup(group)
+  })
 }
 
 function displayTaskGroup(group) {
-    const taskGroupDiv = createDiv('tasks')
-    const header = createH2(format(parseISO(group[0].date), 'd MMM yyyy'))
-    taskGroupDiv.appendChild(header)
-    group.forEach(task => taskGroupDiv.appendChild(createTaskForm(task)));
+  const taskGroupDiv = createDiv('tasks')
+  const header = createH2(format(parseISO(group[0].date), 'd MMM yyyy'))
+  taskGroupDiv.appendChild(header)
+  group.forEach(task => taskGroupDiv.appendChild(createTaskForm(task)))
 
-    main.appendChild(taskGroupDiv)
+  main.appendChild(taskGroupDiv)
 }
 
 function createTaskForm(task) {
-    const type = Types[task.type];
-    const taskForm = createForm('task')
+  const type = Types[task.type]
+  const taskForm = createForm('task')
 
-    const radioWrap = createRadioTypeBtn();
+  const radioWrap = createRadioTypeBtn()
 
-    const p = createP('task_name', task.name)
-    p.classList.add('flex-grow')
+  const p = createP('task_name', task.name)
+  p.classList.add('flex-grow')
 
-    const rightDiv = createTimeAndFinishDiv();
+  const rightDiv = createTimeAndFinishDiv()
 
-    taskForm.appendChild(radioWrap)
-    taskForm.appendChild(p)
-    taskForm.appendChild(rightDiv)
-    return taskForm;
+  taskForm.appendChild(radioWrap)
+  taskForm.appendChild(p)
+  taskForm.appendChild(rightDiv)
+  return taskForm
 
-    function createRadioTypeBtn() {
-        const radioWrap = createDiv('radio_wrap')
-        const radioBtn = createRadioBtn(type.btnColor, 'type', 'type', 'type')
-        radioBtn.disabled = true;
-        radioWrap.appendChild(radioBtn);
-        return radioWrap;
-    }
+  function createRadioTypeBtn() {
+    const radioWrap = createDiv('radio_wrap')
+    const radioBtn = createRadioBtn(type.btnColor, 'type', 'type', 'type')
+    radioBtn.disabled = true
+    radioWrap.appendChild(radioBtn)
+    return radioWrap
+  }
 
-    function createTimeAndFinishDiv() {
-        const rightDiv = createDiv('task_time_btn_container')
-        const time = createP('task_time', convertTo12HourFormat(task.time));
-        const finishBtnWrap = createDiv('radio_wrap')
-        const finishBtn = createBtn('submit', 'finish_btn', '')
-        finishBtn.setAttribute('taskId', task.id)
-        finishBtn.addEventListener('click', deleteTaskEventListener)
-        finishBtnWrap.appendChild(finishBtn);
+  function createTimeAndFinishDiv() {
+    const rightDiv = createDiv('task_time_btn_container')
+    const time = createP('task_time', convertTo12HourFormat(task.time))
+    const finishBtnWrap = createDiv('radio_wrap')
+    const finishBtn = createBtn('submit', 'finish_btn', '')
+    finishBtn.setAttribute('taskId', task.id)
+    finishBtn.addEventListener('click', deleteTaskEventListener)
+    finishBtnWrap.appendChild(finishBtn)
 
-        rightDiv.appendChild(time)
-        rightDiv.appendChild(finishBtnWrap)
-        return rightDiv;
-    }
+    rightDiv.appendChild(time)
+    rightDiv.appendChild(finishBtnWrap)
+    return rightDiv
+  }
 }
--- src/dom/sidePanelDom.js
+++ src/dom/sidePanelDom.js
@@ -1,61 +1,61 @@
-import CalendarClockIcon from "../assets/img/calendar_clock.svg";
-import EventIcon from "../assets/img/event.svg";
-import {createDiv, createP, createSpan} from "./domUtils";
-import {showScheduledTasks} from "../logic/scheduledTasks";
-import renderTodayMainPanelDom from "./todayPanelDom";
+import CalendarClockIcon from '../assets/img/calendar_clock.svg'
+import EventIcon from '../assets/img/event.svg'
+import { createDiv, createP, createSpan } from './domUtils'
+import { showScheduledTasks } from '../logic/scheduledTasks'
+import renderTodayMainPanelDom from './todayPanelDom'
 
 const sideMenu = document.querySelector('#side_menu')
 
 function createCategoryListElement(text) {
-    const li = document.createElement('li');
-    const span = createSpan('clickable_text', text)
-    li.appendChild(span);
-    return li;
+  const li = document.createElement('li')
+  const span = createSpan('clickable_text', text)
+  li.appendChild(span)
+  return li
 }
 
 function addScheduledTasksPanel() {
-    const scheduledTasksCard = createDiv('side_menu_card')
-    const imgWrap = createDiv('img_wrap')
-    imgWrap.addEventListener('click', showScheduledTasks)
-    const img = document.createElement('img')
-    img.classList.add('clickable_text')
-    img.alt = 'Calendar icon'
-    img.src = CalendarClockIcon;
-    const p = createP('clickable_text', 'Scheduled tasks')
-    p.addEventListener('click', showScheduledTasks)
+  const scheduledTasksCard = createDiv('side_menu_card')
+  const imgWrap = createDiv('img_wrap')
+  imgWrap.addEventListener('click', showScheduledTasks)
+  const img = document.createElement('img')
+  img.classList.add('clickable_text')
+  img.alt = 'Calendar icon'
+  img.src = CalendarClockIcon
+  const p = createP('clickable_text', 'Scheduled tasks')
+  p.addEventListener('click', showScheduledTasks)
 
-    imgWrap.appendChild(img);
-    scheduledTasksCard.appendChild(imgWrap);
-    scheduledTasksCard.appendChild(p);
-    sideMenu.appendChild(scheduledTasksCard)
+  imgWrap.appendChild(img)
+  scheduledTasksCard.appendChild(imgWrap)
+  scheduledTasksCard.appendChild(p)
+  sideMenu.appendChild(scheduledTasksCard)
 }
 
 function addTodayTasksPanel() {
-    const todayTasksCard = createDiv('side_menu_card')
-    const imgWrap = createDiv('img_wrap')
-    imgWrap.addEventListener('click', renderTodayMainPanelDom)
-    const img = document.createElement('img')
-    img.classList.add('clickable_text')
-    img.alt = 'Calendar icon'
-    img.src = EventIcon;
-    const p = createP('clickable_text', 'Today tasks')
-    p.addEventListener('click', renderTodayMainPanelDom)
-    const ul = document.createElement('ul');
-    const home = createCategoryListElement('Home')
-    const work = createCategoryListElement('Work')
-    const personal = createCategoryListElement('Personal')
-    ul.appendChild(home)
-    ul.appendChild(work)
-    ul.appendChild(personal)
+  const todayTasksCard = createDiv('side_menu_card')
+  const imgWrap = createDiv('img_wrap')
+  imgWrap.addEventListener('click', renderTodayMainPanelDom)
+  const img = document.createElement('img')
+  img.classList.add('clickable_text')
+  img.alt = 'Calendar icon'
+  img.src = EventIcon
+  const p = createP('clickable_text', 'Today tasks')
+  p.addEventListener('click', renderTodayMainPanelDom)
+  const ul = document.createElement('ul')
+  const home = createCategoryListElement('Home')
+  const work = createCategoryListElement('Work')
+  const personal = createCategoryListElement('Personal')
+  ul.appendChild(home)
+  ul.appendChild(work)
+  ul.appendChild(personal)
 
-    imgWrap.appendChild(img);
-    todayTasksCard.appendChild(imgWrap);
-    todayTasksCard.appendChild(p)
-    todayTasksCard.appendChild(ul)
-    sideMenu.appendChild(todayTasksCard);
+  imgWrap.appendChild(img)
+  todayTasksCard.appendChild(imgWrap)
+  todayTasksCard.appendChild(p)
+  todayTasksCard.appendChild(ul)
+  sideMenu.appendChild(todayTasksCard)
 }
 
-export default function renderSidePanelDom(){
-    addTodayTasksPanel();
-    addScheduledTasksPanel();
+export default function renderSidePanelDom() {
+  addTodayTasksPanel()
+  addScheduledTasksPanel()
 }
